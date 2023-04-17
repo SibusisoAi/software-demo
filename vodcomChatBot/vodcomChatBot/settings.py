f634929fe32a2b23a73d73294d8c65939f525f10
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2023-04-17 18:10:31 +0200

    chore: added new cors header setting

--- vodcomChatBot/vodcomChatBot/settings.py
+++ vodcomChatBot/vodcomChatBot/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'vodcomChatBot.urls'
